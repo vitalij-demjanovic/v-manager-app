db38f87bb835f256ecdffcc86485bf6040d33f53
vitalij_demjanovic <vitalij.demjanovic@maisondidee.com>
2024-10-16 23:17:55 +0200

    Update/layout font & color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#94A3B8'
       },
     },
   },
