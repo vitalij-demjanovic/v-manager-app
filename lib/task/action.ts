c7e5244b69255aaa9997098c2669bd099226c60e
vitalij_demjanovic <vitalij.demjanovic@maisondidee.com>
2024-10-19 23:44:12 +0200

    Feature - create task action

--- lib/task/action.ts
+++ lib/task/action.ts
@@ -1,1 +1,21 @@
 'use server';
+
+import { TaskPriority, TaskProgress } from '@prisma/client';
+import { createTask } from '@/lib/task/task';
+
+export async function createTaskAction(task: FormData, projectId: number) {
+  try {
+    const name = task.get('name') as string;
+    const priority = task.get('priority') as TaskPriority;
+    const progress = task.get('progress') as TaskProgress;
+
+    await createTask({
+      projectId,
+      name,
+      priority,
+      progress
+    });
+  } catch (error) {
+    console.error('Error creating project:', error);
+  }
+}
